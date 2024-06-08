8b36e171841bc4888ec2abf1774524372ccf4236
Eugeny Kurnysh <garted93@gmail.com>
2024-06-08 15:59:42 +0300

    add localStorage
    lc

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import RickAndMorty from "./pages/rickadnmorty/RickAndMorty";
 import Createx from "./pages/createx/Createx";
 import Arrowflicks from "./pages/arrowflicks/Arrowflicks";
 import Page40 from "./pages/asdd/Pag4";
+import { useEffect } from "react";
 
 import { HashRouter as BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+    useEffect(() => {
+        if (!localStorage.getItem("localData")) {
+            localStorage.setItem("localData", JSON.stringify([]));
+        }
+    }, []);
     return (
         <div className="App">
             <BrowserRouter>
